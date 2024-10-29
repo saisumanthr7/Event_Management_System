a39ed738183d6fb46e82e2c4c541290d0f1ad6cf
sai sumanth reddy gurrala <saisumanthreddy76@gmail.com>
2024-10-29 16:21:35 -0500

    Added Spring Security to application
    1. Created User Entity with certain fields.
    2. Created Enum to create roles for the user entity.
    3. Added user repository to talk to the database with a method to find the user using email.
    4. Created a security configuration file to configure the security using web security and add security filter chains.
    5. Added Jwt service to create and validate jwt tokens.
    6. Added JWT auth filter to filter the authentication process based on the user details and jwt token.
    7. Added application config to configure the auth manager and auth provider and also search the user based on the email. We are also encoding the password using BCrypt.
    8. Added necessary dependencies to create json web tokens.

---
+++ Server-EMS/src/main/java/com/example/Event/Management/System/Configuration/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.example.Event.Management.System.Configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final AuthenticationProvider authenticationProvider;
+    private final JWTAuthFilter jwtAuthFilter;
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers("/EMS/auth/*").permitAll()
+                                .requestMatchers("EMS/auth/authenticate").permitAll()
+                                .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+}
---
+++ Server-EMS/src/main/java/com/example/Event/Management/System/Enums/Roles.java
@@ -0,0 +1,8 @@
+package com.example.Event.Management.System.Enums;
+
+
+public enum Roles {
+    MEMBER,
+    ADMIN,
+    ORGANIZER
+}
