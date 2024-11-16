693f45342633b6b964b35b7e56370ab6c0fc76f8
saisumanthr7 <saisumanthreddy76@gmail.com>
2024-11-15 22:52:13 -0600

    Implemented the event service
    1. I have implemented the event service to add, update, retrieve and delete the events.

--- Server-EMS/src/main/java/com/example/Event/Management/System/Service/EventService/EventServiceImpl.java
+++ Server-EMS/src/main/java/com/example/Event/Management/System/Service/EventService/EventServiceImpl.java
@@ -1,4 +1,35 @@
 package com.example.Event.Management.System.Service.EventService;
+import com.example.Event.Management.System.Entity.Event;
+import com.example.Event.Management.System.Repository.Event.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class EventServiceImpl {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    public Event saveEvent(Event event) {
+        return eventRepository.save(event);
+    }
+
+    public List<Event> getAllEvents() {
+        return eventRepository.findAll();
+    }
+
+    public Optional<Event> getEventById(Long id) {
+        return eventRepository.findById(id);
+    }
+
+    public void deleteEvent(Long id) {
+        if (eventRepository.existsById(id)) {
+            eventRepository.deleteById(id);
+        } else {
+            throw new IllegalArgumentException("Event with ID " + id + " does not exist.");
+        }
+    }
 }
